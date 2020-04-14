a64dd2dc80c94867e828de6877964483f898106e
Backsauce <gk.42@hotmail.com>
2020-04-14 01:07:41 -0400

    title shit
    afguquiojgwe

---
+++ Assets/Scripts/Menus/Title/TitleGoop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleGoop : MonoBehaviour
+{
+    public float speed, speedMin, speedMax;
+    public int color;
+    public Vector3 direction;
+    private GameObject spawner;
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(speedMin, speedMax);
+        spawner = GameObject.FindWithTag("TitleSpawner");
+
+        animator.SetInteger("Color", color);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Menus/Title/TitleSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSpawner : MonoBehaviour
+{
+    public GameObject goop;
+    public Vector3 spawnPosL, spawnPosR;
+    public int direction, color;
+    public float timer, timerMin, timerMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(timerMin, timerMax);
+        direction = Random.Range(0, 2);
+        color = Random.Range(0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0){
+            if(direction == 0){
+                var currentGoop = Instantiate(goop, spawnPosL, Quaternion.identity);
+                currentGoop.GetComponent<TitleGoop>().direction.x = 1;
+                currentGoop.GetComponent<TitleGoop>().animator.SetInteger("Color", color);
+                currentGoop.GetComponent<TitleGoop>().animator.SetFloat("Blend", direction);
+
+            } else if (direction == 1)
+            {
+                var currentGoop = Instantiate(goop, spawnPosR, Quaternion.identity);
+                currentGoop.GetComponent<TitleGoop>().direction.x = -1;
+                currentGoop.GetComponent<TitleGoop>().animator.SetInteger("Color", color);
+                currentGoop.GetComponent<TitleGoop>().animator.SetFloat("Blend", direction);
+            }
+
+            timer = Random.Range(timerMin, timerMax);
+            direction = Random.Range(0, 2);
+            color = Random.Range(0, 3);
+        }
+    }
+}
