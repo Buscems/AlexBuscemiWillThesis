1d0808bdab5d5320fbdd93233d76d476903c5a89
Buscems <46265895+Buscems@users.noreply.github.com>
2020-04-14 14:06:37 -0400

    Title Menu is good to go

---
+++ Assets/Scripts/Menus/Title/TitleButtons.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class TitleButtons : MonoBehaviour
+{
+
+    public EventSystem es;
+    public GameObject startButton;
+
+    public Animator fadeGame;
+    public Animator fadeQuit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        fadeGame.SetTrigger("Fade");
+    }
+
+    public void QuitButton()
+    {
+        fadeQuit.SetTrigger("Fade");
+    }
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("Pablo");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetFirstButton()
+    {
+        es.SetSelectedGameObject(startButton);
+    }
+
+}
