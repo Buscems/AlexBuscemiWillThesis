86bbb953310ba9a8781b4924383d5b324633258d
Buscems <46265895+Buscems@users.noreply.github.com>
2020-04-14 17:07:34 -0400

    Here we go for real

--- Assets/Scripts/Menus/Title/TitleButtons.cs
+++ Assets/Scripts/Menus/Title/TitleButtons.cs
@@ -24,7 +24,10 @@ public class TitleButtons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Cursor.visible)
+        {
+            Cursor.visible = false;
+        }
     }
 
     public void StartButton()
